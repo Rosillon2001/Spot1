2d5a9e23f66e680e2d89ff60654bb1abe7cbf997
rosillon <rosillon2001@gmail.com>
2021-07-03 15:11:35 -0400

    a5

--- backend/routes/usermgn.js
+++ backend/routes/usermgn.js
@@ -3,30 +3,45 @@ const router = require('express').Router();
 const user = require('../models/users.js')
 
 router.get('/login', (req, res)=>{
-    res.render('partials/login');
+    res.render('partials/login')
 });
 
 router.get('/register', (req, res)=>{
     res.render('partials/register');
 });
 
+
+
 router.post('/register', async(req, res)=>{
     console.log(req.body);
-    const {username, password, type} = req.body;
+    const {username, password, passwordConf, type} = req.body;
     //console.log(username);
     //console.log(password);
 
-    //manejo de errores
+    //consultas
+    const usersDB = await user.find('username: `{$username}`' );
+    console.log(userDB);
+
+    //manejo de errores y exito
     const errors=[];
+    const success=[];
+
     if(!username){
         errors.push({text: "Por favor ingrese un usuario"});
     }
     if(!password){
         errors.push({text: "Por favor ingrese una clave"});
     }
+    if(password!=passwordConf){
+        errors.push({text: "Las contraseñas no coinciden"});
+    }
     if(errors.length>=1){
-        res.render('partials/register')
-    }else{
+        res.render('partials/register',{
+            errors
+        });
+    }
+    else{
+        success.push({text: "Registro exitoso"});
         const newuser = new user({username, password, type});
         await newuser.save();
         res.redirect('/login');
@@ -35,7 +50,30 @@ router.post('/register', async(req, res)=>{
 
 
 router.post('/login', async (req, res) => {
+    console.log(req.body);
+    const {username, password} = req.body;
 
+    //validacion del usuario para el login
+    const usersDB = await user.find({username: `${username}`});
+    console.log(usersDB); 
+    let userFlag=0;
+
+    for(let i=0;i<usersDB.length;i++){
+        let userInDB = usersDB[i].username;
+        console.log(userInDB)
+        if(username==userInDB){
+            console.log('El usuario existe, iniciando');
+            userFlag=1;
+        }else{
+            userFlag=0;
+        }
+    }
+    if(userFlag==0){
+        console.log('El usuario no existe');
+    }else{
+        res.render('partials/admin');
+    }
+    
 });
 
 module.exports=router;
