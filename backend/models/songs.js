d977806e7e9dc36bb203b47664fa2a610541f1aa
rosillon <rosillon2001@gmail.com>
2021-07-07 14:41:10 -0400

    add navbar admin

---
+++ backend/models/songs.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const{Schema} = mongoose;
+
+const song = new Schema({
+    name: {type:String, required:true}, 
+    author: {type:String, required:true},
+    gender: {type:String, required:true}, 
+    album: {type:String, required:true}, 
+    route: {type:String, required:true}
+});
+
+module.exports = mongoose.model('songs', song);
